58f522ad475a058335654106a78a73d0d221c7f4
Kevin Snijders <kevin_snijders@live.nl>
2019-12-29 14:16:54 +0100

    Add onClick prop to custom button

--- src/components/custom-button/CustomButton.component.tsx
+++ src/components/custom-button/CustomButton.component.tsx
@@ -3,15 +3,19 @@ import React from "react";
 import { CustomButtonContainer } from "./CustomButton.styles";
 
 type CustomButtonProps = {
-  type: string;
+  type?: string;
+  onClick?: () => void;
   children?: React.ReactNode;
 };
 
 const CustomButton: React.FC<CustomButtonProps> = ({
   type,
+  onClick,
   children
 }: CustomButtonProps) => (
-  <CustomButtonContainer type={type}>{children}</CustomButtonContainer>
+  <CustomButtonContainer type={type} onClick={onClick}>
+    {children}
+  </CustomButtonContainer>
 );
 
 export default CustomButton;
