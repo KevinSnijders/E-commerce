853e1aa014d634264ea817ae4600f80d79c6ed1d
Kevin Snijders <kevin_snijders@live.nl>
2020-01-05 19:04:26 +0100

    Add firebase util function

--- src/firebase/firebase.utils.ts
+++ src/firebase/firebase.utils.ts
@@ -44,6 +44,40 @@ export const createUserProfileDocument = async (
   return userRef;
 };
 
+export const addCollectionAndDocuments: any = async (
+  collectionKey: any,
+  objectsToAdd: any
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+
+  objectsToAdd.forEach((obj: any) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  await batch.commit();
+};
+
+export const convertCollectionToMap: any = (collections: any) => {
+  const transformedCollection = collections.docs.map((doc: any) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((acc: any, collection: any) => {
+    acc[collection.title.toLowerCase()] = collection;
+    return acc;
+  }, {});
+};
+
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
