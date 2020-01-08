faa619ae2e2bd8f7d765cdd2cb8645b978bd6ff4
Kevin Snijders <kevin_snijders@live.nl>
2020-01-08 12:08:50 +0100

    Refactored the interface naming and location

--- src/components/collections-overview/CollectionsOverview.component.tsx
+++ src/components/collections-overview/CollectionsOverview.component.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
-import {
-  Shop as IShop,
-  Collection,
-  CollectionList
-} from "../../redux/shop/shopReducer";
+import { Shop as ShopState } from "../../redux/rootReducer";
+import { Collection, Collections } from "../../redux/shop/shopReducer";
+
 import { selectCollectionsForPreview } from "../../redux/shop/shopSelector";
 import { CollectionsOverviewContainer } from "./CollectionsOverview.styles";
 
 import CollectionPreview from "../collection-preview/CollectionPreview.component";
 
-const mapStateToProps = (state: IShop) => ({
+const mapStateToProps = (state: ShopState) => ({
   collections: selectCollectionsForPreview(state)
 });
 
-const CollectionsOverview = ({ collections }: CollectionList) => {
+const CollectionsOverview = ({ collections }: Collections) => {
   return (
     <CollectionsOverviewContainer>
       {collections.map(({ id, title, routeName, items }: Collection) => (
